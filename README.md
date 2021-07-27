1fd6874a4d8a2728f976607ae2c0d456c4086c9d
KRM7 <70973547+KRM7@users.noreply.github.com>
2021-07-27 17:42:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Quasi-random number generator
+
+<p>A simple quasi-random number generator implementation in C++ for generating low-discrepancy sequences. <br>
+It generates points in any number of dimensions on a unit hypercube.  
+</p>
+
+The generator is based on:  
+&nbsp;&nbsp; *Martin Roberts, 2018. "[The Unreasonable Effectiveness of Quasirandom Sequences](http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/)"*  
+
+## Usage example
+
+```cpp
+#include "quasirand.h"
+
+using namespace quasirand;
+
+int main()
+{
+    /* Initialize the generator in 3 dimensions. */
+    QuasiRandom qrng(3);
+
+    /* Generate the next point of the sequence. */
+    vector<double> point = qrng();
+
+    /* Generate the 500th point of the sequence. */
+    vector<double> point500 = qrng(500);
+
+    /* Discard the next point from the sequence. */
+    qrng.discard();
+
+    /* Set a seed different from the default one. */
+    qrng.seed(0.13);
+
+    return 0;
+}
+```
+
+## Plots showing some generated sequences
+
+#### <center>2-dimensions</center>
+
+|   ![](plots/2d_50.png)	|   ![](plots/2d_100.png)	|
+|           ---	            |           ---	            |
+|   ![](plots/2d_500.png)	|   ![](plots/2d_2000.png)	|
+
+#### <center>3-dimensions</center>
+
+|   ![](plots/3d_50.png)	|   ![](plots/3d_100.png)	|
+|           ---	            |           ---	            |
+|   ![](plots/3d_500.png)	|   ![](plots/3d_2000.png)	|
---
+++ quasirand.h
@@ -0,0 +1,96 @@
+/*
+* A simple quasi-random number generator implementation for generating low-discrepancy
+* sequences.
+* 
+* See: Martin Roberts, 2018. "The Unreasonable Effectiveness of Quasirandom Sequences", 
+* http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/
+*/
+
+#ifndef QUASIRAND_H
+#define QUASIRAND_H
+
+#include <cmath>
+#include <vector>
+#include <stdexcept>
+
+namespace quasirand
+{
+    /*
+    * Quasi-random number generator based on: http://extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences/
+    * Generates points on the unit hypercube in dim dimensions.
+    */
+    class QuasiRandom
+    {
+    public:
+
+        /* Construct the generator in dim dimensions with a seed. */
+        QuasiRandom(size_t dim) : dim(dim)
+        {
+            if (dim == 0)
+            {
+                throw std::invalid_argument("The dimension of the generator must be at least 1.");
+            }
+
+            alpha.reserve(dim);
+            point.reserve(dim);
+            double phid = phi(dim);
+            for (size_t i = 0; i < dim; i++)
+            {
+                alpha.push_back(1.0/pow(phid, i + 1));
+                point.push_back(s);
+            }
+        }
+
+        /* Generate the next random point in the sequence. */
+        std::vector<double> operator()()
+        {
+            for (size_t i = 0; i < point.size(); i++)
+            {
+                point[i] += alpha[i];
+                point[i] -= floor(point[i]);
+            }
+            return point;
+        }
+
+        /* Generate the n-th point in the sequence. */
+        std::vector<double> operator()(size_t n) const
+        {
+            std::vector<double> nth_point(dim);
+            for (size_t i = 0; i < nth_point.size(); i++)
+            {
+                nth_point[i] = s + alpha[i] * n;
+                nth_point[i] -= floor(nth_point[i]);
+            }
+            return nth_point;
+        }
+
+        /* Discard the next n points of the sequence. */
+        void discard(size_t n = 1)
+        {
+            for (size_t i = 0; i < n; i++)
+            {
+                operator()();
+            }
+        }
+
+        /* Set the seed of the generator. */
+        void seed(double seed)
+        {
+            s = seed;
+        }
+
+    private:
+        const size_t dim;
+        double s = 0.5;
+        std::vector<double> alpha;
+        std::vector<double> point;
+
+        /* Approximate the generalized golden ratio in dim dimensions. */
+        static double phi(size_t dim, size_t depth = 30)
+        {
+            if (depth == 0) return 1.0;
+            else return pow(1.0 + phi(dim, depth - 1), 1.0/(dim + 1.0));
+        }
+    };
+} // namespace quasirand
+#endif
